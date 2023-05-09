b23b36f2fe96be2a1655a3ab8cc3f13f8c5a4e38
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-05-09 12:09:04 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// Thank you cards
+const friends =["Charlie", "Samip", "Ali"]
+const gift = ["birthday"]
+
+function writeCards( friends , gift) {
+    let greetings = []
+    for (let i = 0; i < friends.length; i++) {
+        console.log(`Thank you, ${friends[i]}, for the wonderful ${gift} gift!`);
+        greetings.push(`Thank you, ${friends[i]}, for the wonderful ${gift} gift!`);
+    }
+    return greetings;
+}
+writeCards(friends, gift);
+
+// 
+function countDown() {
+for (let countDown = 10; countDown > -1; countDown--) {
+    console.log(countDown);
+    }
+}
